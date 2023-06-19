db2430d98844ddc1b97ae4bfb2e35557761c0ebc
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-06-19 21:08:00 +1000

    contact update

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,5 +1,8 @@
 import "./Contact.css";
 
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 export const Contact = () => {
   return (
     <div id="contact" class="container contact rounded-5 py-2">
@@ -17,6 +20,41 @@ export const Contact = () => {
             <i class="fa-brands fa-linkedin"></i>
           </a>
         </div>
+      </div>
+      <h3 className="text-center">or</h3>
+      <div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="email" placeholder="Enter Your Name" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter phone number"
+              maxLength={10}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as="textarea"
+              type="text"
+              placeholder="Message"
+              maxLength={500}
+              required
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     </div>
   );
